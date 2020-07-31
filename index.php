eede130926f390862c01b31da0f4d28fe6ae489f
unknown <simonmusmos@gmail.com>
2020-07-31 15:01:14 +0800

    uploading to live

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+    <head>
+        
+        <link rel="stylesheet" href="css/aos.css">
+        <link rel="stylesheet" href="css/style1.css">
+        
+    </head>
+    <body>
+        <div id="hero" class="jumbotron" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-delay="100" style='width: 100%;'>
+        <!-- <div class="container"> -->
+            <div class="row align-items-center h-100" style="margin-left: 0; margin-right: 0; width: 100%">
+                <div class="col-lg-6 col-md-12 order-1" style="padding:0">
+                    <h1 class="hero-title" data-aos="fade-right" data-aos-easing="ease-in-out" data-aos-delay="200">Hi, my name is <span class="text-color-main">Simon Musmos</span>,<br>I'm a Young Developer.</h1>
+                    <p class="hero-cta" data-aos="fade-right"data-aos-easing="ease-in-out" data-aos-delay="300">
+                        <a class="cta-btn cta-btn--hero">
+                            Know More
+                        </a>
+                    </p>
+                </div>
+                <div class="col-lg-6 col-md-12 order-2" style="margin-left: 0; padding: 0">
+                    <img src="img/bg-hero.jpg" style="width: 100%">
+                </div>
+            </div>
+        </div>
+        <!-- </div> -->
+        <section id="about" data-aos="fade-up" data-aos-easing="ease-in-out" style="height: 650px">
+            <div class="container">
+                <h2 class="section-title" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-delay="200">About me</h2>
+                <div class="row about-wrapper">
+                    <div class="col-md-6 col-sm-12">
+                        <div class="about-wrapper__image" data-aos="fade-right" data-aos-easing="ease-in-out" data-aos-delay="400">
+                            <img class="img-fluid rounded shadow-lg" height="auto" width="300px" src="img/profile1.jpg" alt="Profile Image">
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <div class="about-wrapper__info">
+                            <p class="about-wrapper__info-text" data-aos="fade-left" data-aos-easing="ease-in-out" data-aos-delay="400">A dependable and hardworking Back-end Developer with a Bachelor’s
+Degree in the course of Information Technology. A good knowledge in
+software and web development. Flexible and can easily adjust on any
+given task or job
+.</p>
+                            <p class="about-wrapper__info-text" data-aos="fade-left" data-aos-easing="ease-in-out" data-aos-delay="550">Currently working as a Web Developer at Dynamic Markups IT Solutions. Most of the task
+revolves around economical transactions, meaning it is about money. The job is very
+crucial, because there must be no line of error since it is about money. Also
+doing Frontend little by little because I want to be a Full-stack Developer
+soon. Most of the projects were made using Native PHP and sometimes Laravel,
+though I am still beginner.</p>
+                            <span class="d-flex mt-3" data-aos="fade-left" data-aos-easing="ease-in-out" data-aos-delay="650"> 
+                                <a target="_blank" class="cta-btn cta-btn--resume" href="resume/SimonMusmos.pdf" target="_blank">View Resume</a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <script src="js/main.js"></script>
+        <!-- <script src="js/vendor.js"></script> -->
+        <script type="text/javascript" src="js/aos.js"></script>
+        <script type="text/javascript">
+            AOS.init();
+        </script>
+        <span>Hardcoded by yours truly.</span>
+    </body>
+</html>
